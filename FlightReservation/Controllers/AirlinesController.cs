2f6ad4a11b30ad69e00ba4969c0ad201bddf541c
Ahmet Akif Kasım <ahmetakifkasim61@gmail.com>
2024-01-25 23:34:56 +0300

    son düzenlemeler, iyzico olmadı

---
+++ FlightReservation/Controllers/AirlinesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FlightReservation.Data;
+
+namespace FlightReservation.Controllers
+{
+    public class AirlinesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AirlinesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Airlines
+        public async Task<IActionResult> Index()
+        {
+              return _context.Airlines != null ? 
+                          View(await _context.Airlines.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Airlines'  is null.");
+        }
+
+        // GET: Airlines/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Airlines == null)
+            {
+                return NotFound();
+            }
+
+            var airline = await _context.Airlines
+                .FirstOrDefaultAsync(m => m.AirlineID == id);
+            if (airline == null)
+            {
+                return NotFound();
+            }
+
+            return View(airline);
+        }
+
+        // GET: Airlines/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Airlines/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AirlineID,AirlineName,IsStatus")] Airline airline)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(airline);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(airline);
+        }
+
+        // GET: Airlines/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Airlines == null)
+            {
+                return NotFound();
+            }
+
+            var airline = await _context.Airlines.FindAsync(id);
+            if (airline == null)
+            {
+                return NotFound();
+            }
+            return View(airline);
+        }
+
+        // POST: Airlines/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AirlineID,AirlineName,IsStatus")] Airline airline)
+        {
+            if (id != airline.AirlineID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(airline);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AirlineExists(airline.AirlineID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(airline);
+        }
+
+        // GET: Airlines/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Airlines == null)
+            {
+                return NotFound();
+            }
+
+            var airline = await _context.Airlines
+                .FirstOrDefaultAsync(m => m.AirlineID == id);
+            if (airline == null)
+            {
+                return NotFound();
+            }
+
+            return View(airline);
+        }
+
+        // POST: Airlines/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Airlines == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Airlines'  is null.");
+            }
+            var airline = await _context.Airlines.FindAsync(id);
+            if (airline != null)
+            {
+                _context.Airlines.Remove(airline);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AirlineExists(int id)
+        {
+          return (_context.Airlines?.Any(e => e.AirlineID == id)).GetValueOrDefault();
+        }
+    }
+}
--- FlightReservation/Controllers/HomeController.cs
+++ FlightReservation/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using FlightReservation.Data;
 using FlightReservation.Models;
+using Iyzipay;
+using Iyzipay.Model;
+using Iyzipay.Request;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -97,6 +100,117 @@ namespace FlightReservation.Controllers
             return View(flights);
         }
 
+        public async Task<IActionResult> CommonPurchase(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var flight = await _context.Flights
+                .Include(f => f.Airline)
+                .Include(f => f.DepartureAirport)
+                .Include(f => f.ArrivalAirport)
+                .FirstOrDefaultAsync(f => f.FlightID == id);
+
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            return View(flight);
+        }
+
+        public IActionResult Order()
+        {
+            Options options = new Options(); // Iyzico Import
+            options.ApiKey = "sandbox-ZDtW3lzihIjLVRBEvyccHn1n8FxQwvwR";
+            options.SecretKey = "sandbox-6HuobXnHRvJA2Zkzzn8WkZ030wREchvI";
+            options.BaseUrl = "Https://sandbox-api.iyzipay.com";
+
+            //double savePrice = 0;
+            //double delivaryShippingPrice = 38;
+            //foreach (var item in GetCart().CartLines)
+            //{
+            //    savePrice += ((item.Quantity * item.Advert.Price) / 100) * campaignRepository.Detail(item.Advert.CampaignId).Rate;
+            //}
+            //double Price = GetCart().TotalPrice() - savePrice + delivaryShippingPrice;
+            //string TotalPrice = Math.Round(Price, 2).ToString().Replace(',', '.');
+
+            //var user = userRepository.UserAccount(User.Identity.Name);
+            //var userShippingAddress = addressRepository.List().FirstOrDefault(x => x.UserId == user.Id && x.IsSelected == true);
+            //var userProvince = provinceRepository.Detail(userShippingAddress.ProvinceId);
+
+
+            CreateCheckoutFormInitializeRequest request = new CreateCheckoutFormInitializeRequest();
+            request.Locale = Locale.TR.ToString();
+            request.ConversationId = "123456789";
+            request.Price = "1";
+            request.PaidPrice = "1";
+            request.Currency = Currency.TRY.ToString();
+            request.BasketId = "B67832";
+            request.PaymentGroup = PaymentGroup.PRODUCT.ToString();
+            request.CallbackUrl = "https://localhost:7178/Customer/Cart/Success";
+
+            //List<int> enabledInstallments = new List<int>();
+            //enabledInstallments.Add(2);
+            //enabledInstallments.Add(3);
+            //enabledInstallments.Add(6);
+            //enabledInstallments.Add(9);
+            //request.EnabledInstallments = enabledInstallments;
+
+            Buyer buyer = new Buyer();
+            buyer.Id = "asdadsada";
+            buyer.Name = "Erhan";
+            buyer.Surname = "Kaya";
+            buyer.GsmNumber = "+905554443322";
+            buyer.Email = "email@email.com";
+            buyer.IdentityNumber = "74300864791";
+            buyer.LastLoginDate = "2015-10-05 12:43:35";
+            buyer.RegistrationDate = "2000-12-12 12:00:00";
+            buyer.RegistrationAddress = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+            buyer.Ip = "85.34.78.112";
+            buyer.City = "Istanbul";
+            buyer.Country = "Turkey";
+            buyer.ZipCode = "34732";
+            request.Buyer = buyer;
+
+            Address shippingAddress = new Address();
+            shippingAddress.ContactName = "Erhan Kaya";
+            shippingAddress.City = "Istanbul";
+            shippingAddress.Country = "Turkey";
+            shippingAddress.Description = "Bereket döner karşısı";
+            shippingAddress.ZipCode = "34742";
+            request.ShippingAddress = shippingAddress;
+
+            Address billingAddress = new Address();
+            billingAddress.ContactName = "Erhan Kaya";
+            billingAddress.City = "Istanbul";
+            billingAddress.Country = "Turkey";
+            billingAddress.Description = "Bereket Döner";
+            billingAddress.ZipCode = "34742";
+            request.BillingAddress = billingAddress;
+
+            List<BasketItem> basketItems = new List<BasketItem>();
+            BasketItem basketProduct;
+            basketProduct = new BasketItem();
+            basketProduct.Id = "1";
+            basketProduct.Name = "Asus Bilgisayar";
+            basketProduct.Category1 = "Bilgisayar";
+            basketProduct.Category2 = "";
+            basketProduct.ItemType = BasketItemType.PHYSICAL.ToString();
+
+            double price = 1;
+            double endPrice = 1;
+            basketProduct.Price = endPrice.ToString().Replace(",", "");
+            basketItems.Add(basketProduct);
+
+            request.BasketItems = basketItems;
+
+            CheckoutFormInitialize checkoutFormInitialize = CheckoutFormInitialize.Create(request, options);
+            ViewBag.pay = checkoutFormInitialize.CheckoutFormContent;
+            return View();
+        }
 
     }
 }
